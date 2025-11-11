c9e6c912a4d36dbf9cce4543061d327d8b054ad3
yucho115 <8villageisfun16@gmail.com>
2025-11-11 15:18:22 +0900

    フォームでメッセージを、メールで送信可

--- diary/forms.py
+++ diary/forms.py
@@ -1,10 +1,17 @@
+#コメント文はメッセージを送信した時の内容確認をログに表示する用
+import os
+#import logging
+
 from django import forms
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前',max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル',max_length=30)
     message = forms.CharField(label='メッセージ',widget=forms.Textarea)
+
+    #logger = logging.getLogger(__name__)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,3 +27,23 @@ class InquiryForm(forms.Form):
 
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ{}'.format(title)
+        message = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name,email,message)
+
+        from_email = os.environ.get('FROM_EMAIL')
+        to_list = [
+            os.environ.get('FROM_EMAIL')
+        ]
+        cc_list = [email]
+
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list,cc=cc_list)
+        message.send()
+
+        #self.logger.info("{}".format(message))
